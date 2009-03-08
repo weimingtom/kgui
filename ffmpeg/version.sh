cdca583d168e73a74e1475e3817e90268562c218
sales <sales@scale18.com>
2009-03-08 18:06:11 +0000

    remove old ffmpeg

--- ffmpeg/version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-NEW_REVISION="#define FFMPEG_VERSION \"SVN-r10832\""
-OLD_REVISION=`cat version.h 2> /dev/null`
-
-# Update version.h only on revision changes to avoid spurious rebuilds
-if test "$NEW_REVISION" != "$OLD_REVISION"; then
-    echo "$NEW_REVISION" > version.h
-fi
